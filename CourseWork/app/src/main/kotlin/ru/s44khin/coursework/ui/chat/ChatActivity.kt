00c8616d96f8e4201de705aa9623869190eb4687
s44khin <s44khin@gmail.com>
2021-10-13 01:16:12 +0800

    initial

---
+++ CourseWork/app/src/main/kotlin/ru/s44khin/coursework/ui/chat/ChatActivity.kt
@@ -0,0 +1,11 @@
+package ru.s44khin.coursework.ui.chat
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ChatActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
