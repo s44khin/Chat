c0953fdaf4a2f14c89653aa2c613889be16daf7f
s44khin <s44khin@gmail.com>
2021-11-08 10:43:11 +0800

    Очень много всего изменил

--- Messenger/app/src/main/kotlin/ru/s44khin/coursework/data/model/Topic.kt
+++
@@ -1,6 +0,0 @@
-package ru.s44khin.coursework.data.model
-
-data class Topic(
-    val name: String,
-    val messages: List<Message>
-)
--- Messenger/app/src/main/kotlin/ru/s44khin/coursework/ui/chat/ChatItem.kt
+++
@@ -1,8 +0,0 @@
-package ru.s44khin.coursework.ui.chat
-
-import ru.s44khin.coursework.data.model.Message
-
-sealed class ChatItem {
-    class MessageItem(val message: Message) : ChatItem()
-    class DateItem(val date: Int) : ChatItem()
-}
